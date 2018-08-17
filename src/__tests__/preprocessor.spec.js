af1870c8a108097a203e80ee158976820dc7b03f
abiyasa <ilham.suhardi@gmail.com>
2018-08-17 17:14:01 +0200

    Support taglibExcludePackages from globalsConfig

--- src/__tests__/preprocessor.spec.js
+++ src/__tests__/preprocessor.spec.js
@@ -1,15 +1,59 @@
+jest.mock('marko/compiler');
+
+const compiler = require('marko/compiler');
+const preprocessor = require('../preprocessor');
+
 describe('preprocessor', () => {
-  const mockedMarkoCompiler = {
-    compileFileForBrowser: jest.fn()
-  };
-  jest.mock('marko/compiler', () => mockedMarkoCompiler);
+  beforeEach(() => {
+    compiler.compileFileForBrowser = jest.fn();
+  });
 
-  // eslint-disable-next-line global-require
-  const preprocessor = require('../preprocessor');
+  afterEach(() => {
+    compiler.compileFileForBrowser.mockRestore();
+  });
 
-  it('should compile Marko file with ouput to vdom', () => {
+  it('should compile Marko file', () => {
     const filepath = 'src/file/path';
+
     preprocessor.process('source', filepath);
-    expect(mockedMarkoCompiler.compileFileForBrowser).toHaveBeenCalledWith(filepath);
+    expect(compiler.compileFileForBrowser).toHaveBeenCalledWith(filepath, undefined);
+  });
+
+  describe('given taglibExcludePackages is defined', () => {
+    const filepath = 'src/file/path';
+    const jestConfig = {
+      globals: {
+        'marko-jest': {
+          taglibExcludePackages: [
+            '@ebay/ebayui-core',
+            'material-marko'
+          ]
+        }
+      }
+    };
+
+    beforeEach(() => {
+      compiler.taglibFinder.excludePackage = jest.fn();
+
+      preprocessor.process('source', filepath, jestConfig);
+    });
+
+    afterEach(() => {
+      compiler.taglibFinder.excludePackage.mockRestore();
+    });
+
+    it('should register excluded packages to marko compiler', () => {
+      expect(compiler.taglibFinder.excludePackage).toHaveBeenCalledTimes(2);
+
+      const { calls } = compiler.taglibFinder.excludePackage.mock;
+      expect(calls).toEqual([['@ebay/ebayui-core'], ['material-marko']]);
+    });
+
+    it('should compile Marko file with the right compiler option', () => {
+      expect(compiler.compileFileForBrowser).toHaveBeenCalledWith(
+        filepath,
+        { ignoreUnrecognizedTags: true }
+      );
+    });
   });
 });
--- src/preprocessor.js
+++ src/preprocessor.js
@@ -2,9 +2,19 @@ const compiler = require('marko/compiler');
 
 // Jest helper to compile Marko
 module.exports = {
-  process(src, filepath) {
+  process(src, filepath, jestCfg = {}) {
+    const markoJestCfg = (jestCfg.globals && jestCfg.globals['marko-jest']) || {};
+
+    let compileOption;
+    const { taglibExcludePackages } = markoJestCfg;
+    if (taglibExcludePackages) {
+      taglibExcludePackages.forEach(package => compiler.taglibFinder.excludePackage(package));
+
+      compileOption = { ignoreUnrecognizedTags: true };
+    }
+
     // NOTE: need to render to vdom so rendering to JSDOM works
-    const compiledSrc = compiler.compileFileForBrowser(filepath);
+    const compiledSrc = compiler.compileFileForBrowser(filepath, compileOption);
     return compiledSrc;
   },
 };
